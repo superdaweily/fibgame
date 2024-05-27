173b194625c6ff08db5678f90c9ee4321e96f347
supersamdev <superdavidsam24@gmail.com>
2024-05-27 08:21:13 -0700

    modified image fetch

--- apps/game1/src/app/lib/convert-image.ts
+++ apps/game1/src/app/lib/convert-image.ts
@@ -1,8 +1,13 @@
 import { s3Upload } from "./aws-s3";
+import fetch from "node-fetch";
 
 export async function convertAndSaveImage(imageUrl: string) {
   try {
-    const imageData = (await fetch(imageUrl)) as any;
+    const response = await fetch(imageUrl);
+    let imageData;
+    if (response.ok) {
+      imageData = response.buffer() as any;
+    }
     const fileName = `image-${Date.now()}.jpg`;
     const s3Image = await s3Upload(imageData, fileName);
     return { url: s3Image.url };
