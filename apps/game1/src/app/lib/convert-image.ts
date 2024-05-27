5b2828b6cacb6bbfc88885e18b7ac34cb147fa04
supersamdev <superdavidsam24@gmail.com>
2024-05-27 07:25:43 -0700

    remove public from url

--- apps/game1/src/app/lib/convert-image.ts
+++ apps/game1/src/app/lib/convert-image.ts
@@ -8,12 +8,9 @@ export async function convertAndSaveImage(imageUrl: string) {
     const response = await fetch(imageUrl);
     const imageData = await response.arrayBuffer();
     const jpgData = await sharp(imageData).toFormat("jpeg").toBuffer();
-    const outputPath = `./public/images/${Date.now()}.jpg`;
+    const outputPath = `./images/${Date.now()}.jpg`;
     fs.writeFileSync(outputPath, jpgData);
-    const fullUrl = join(
-      process.env.SITE_URL!,
-      outputPath.replace("./public", "")
-    );
+    const fullUrl = join(process.env.SITE_URL!, outputPath);
     return { url: fullUrl };
   } catch (error) {
     console.error("Error converting and saving image:", error);
