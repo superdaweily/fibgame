f21bddfc097a74fc30ada6d4934ca37250bb6adb
supersamdev <superdavidsam24@gmail.com>
2024-05-26 19:23:05 -0700

    jimp instead of sharp

--- packages/utils/lib/convert-image.ts
+++ packages/utils/lib/convert-image.ts
@@ -1,19 +1,19 @@
-import sharp from "sharp";
-// import fetch from 'node-fetch';
+import Jimp from "jimp";
+import fetch from "node-fetch";
 import fs from "fs";
 
 export const convertAndSaveImage = async (imageUrl: string) => {
   try {
     // Fetch the image from the provided URL
     const response = await fetch(imageUrl);
-    const imageData = await response.arrayBuffer();
+    const imageData = await response.buffer();
 
-    // Convert the fetched image data from JPEG to JPG format using Sharp
-    const jpgData = await sharp(Buffer.from(imageData)).jpeg().toBuffer();
+    // Convert the fetched image data from JPEG to JPG format using Jimp
+    const image = await Jimp.read(imageData);
+    const outputPath = `./public/images/${Date.now()}.jpg`;
 
     // Save the converted JPG image data to the backend (Next.js public directory)
-    const outputPath = `./public/images/${Date.now()}.jpg`;
-    fs.writeFileSync(outputPath, jpgData);
+    await image.writeAsync(outputPath);
 
     // Return the URL of the saved JPG image
     return { url: `${process.env.BASE_SITE_URL}/${outputPath}` };
