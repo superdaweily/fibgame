126f511aaca9b20215cf052d82d55879a219c018
supersamdev <superdavidsam24@gmail.com>
2024-05-27 08:10:27 -0700

    integrated s3

--- apps/game1/src/app/lib/convert-image.ts
+++ apps/game1/src/app/lib/convert-image.ts
@@ -1,20 +1,11 @@
-import sharp from "sharp";
-// import fetch from 'node-fetch';
-import { join } from "path";
-import fs from "fs";
+import { s3Upload } from "./aws-s3";
 
 export async function convertAndSaveImage(imageUrl: string) {
   try {
-    const response = await fetch(imageUrl);
-    const imageData = await response.arrayBuffer();
-    const jpgData = await sharp(imageData).toFormat("jpeg").toBuffer();
-    const outputPath = `./apps/game1/public/images/${Date.now()}.jpg`;
-    fs.writeFileSync(outputPath, jpgData);
-    const fullUrl = join(
-      process.env.BASE_SITE_URL!,
-      outputPath.replace("./apps/game1/public", "")
-    );
-    return { url: fullUrl };
+    const imageData = (await fetch(imageUrl)) as any;
+    const fileName = `image-${Date.now()}.jpg`;
+    const s3Image = await s3Upload(imageData, fileName);
+    return { url: s3Image.url };
   } catch (error) {
     console.error("Error converting and saving image:", error);
     return null;
